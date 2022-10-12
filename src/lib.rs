84d25a35ae88d9ff0a56dc9eacde0b89c8f1ebe8
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2022-10-13 01:15:57 +0800

    Update metadata.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate alloc;
 
 use polymesh_api_codegen_macro::*;
 
-#[codegen_api(metadata_file = "specs/polymesh_dev_spec_5000002.meta")]
+#[codegen_api(metadata_file = "specs/polymesh_dev_spec_5001001.meta")]
 mod polymesh {}
 
 pub use polymesh::*;
