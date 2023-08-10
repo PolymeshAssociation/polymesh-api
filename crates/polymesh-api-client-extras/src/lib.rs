f7588b76df2d1efc7d65add7ef678ef24f061281
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2023-08-11 01:16:10 +0800

    Add get_identity_id

--- crates/polymesh-api-client-extras/src/lib.rs
+++ crates/polymesh-api-client-extras/src/lib.rs
@@ -3,15 +3,15 @@ use codec::{Decode, Encode};
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
-use polymesh_api::TransactionResults;
 use polymesh_api::client::basic_types::{AccountId, IdentityId};
 use polymesh_api::client::error::Result;
 use polymesh_api::types::{
-  polymesh_primitives::settlement::{VenueId, InstructionId},
-  polymesh_primitives::asset::CheckpointId,
   polymesh_common_utilities::traits::checkpoint::ScheduleId,
+  polymesh_primitives::asset::CheckpointId,
+  polymesh_primitives::settlement::{InstructionId, VenueId},
   runtime::{events::*, RuntimeEvent},
 };
+use polymesh_api::TransactionResults;
 
 pub type Moment = u64;
 pub type AuthorizationNonce = u64;
@@ -39,66 +39,60 @@ pub enum CreatedIds {
 
 /// Get ids from *Created events.
 pub async fn get_created_ids(mut res: TransactionResults) -> Result<Vec<CreatedIds>> {
-  Ok(res.events().await?.map(|events| {
-    let mut ids = Vec::new();
+  Ok(
+    res
+      .events()
+      .await?
+      .map(|events| {
+        let mut ids = Vec::new();
+        for rec in &events.0 {
+          match &rec.event {
+            RuntimeEvent::Settlement(SettlementEvent::VenueCreated(_, id, ..)) => {
+              ids.push(CreatedIds::VenueCreated(*id));
+            }
+            RuntimeEvent::Settlement(SettlementEvent::InstructionCreated(_, _, id, ..)) => {
+              ids.push(CreatedIds::InstructionCreated(*id));
+            }
+            RuntimeEvent::Checkpoint(CheckpointEvent::CheckpointCreated(_, _, id, ..)) => {
+              ids.push(CreatedIds::CheckpointCreated(id.clone()));
+            }
+            RuntimeEvent::Checkpoint(CheckpointEvent::ScheduleCreated(_, _, id, ..)) => {
+              ids.push(CreatedIds::ScheduleCreated(id.clone()));
+            }
+            RuntimeEvent::Identity(IdentityEvent::DidCreated(id, ..)) => {
+              ids.push(CreatedIds::IdentityCreated(*id));
+            }
+            RuntimeEvent::Identity(IdentityEvent::ChildDidCreated(_, id, ..)) => {
+              ids.push(CreatedIds::ChildIdentityCreated(*id));
+            }
+            RuntimeEvent::MultiSig(MultiSigEvent::MultiSigCreated(_, id, ..)) => {
+              ids.push(CreatedIds::MultiSigCreated(*id));
+            }
+            _ => (),
+          }
+        }
+        ids
+      })
+      .unwrap_or_default(),
+  )
+}
+
+/// Search transaction events for newly created identity id.
+pub async fn get_identity_id(mut res: TransactionResults) -> Result<Option<IdentityId>> {
+  Ok(res.events().await?.and_then(|events| {
     for rec in &events.0 {
       match &rec.event {
-        RuntimeEvent::Settlement(SettlementEvent::VenueCreated(
-          _,
-          id,
-          ..
-        )) => {
-          ids.push(CreatedIds::VenueCreated(*id));
+        RuntimeEvent::Identity(IdentityEvent::DidCreated(id, ..)) => {
+          return Some(*id);
         }
-        RuntimeEvent::Settlement(SettlementEvent::InstructionCreated(
-          _,
-          _,
-          id,
-          ..
-        )) => {
-          ids.push(CreatedIds::InstructionCreated(*id));
-        }
-        RuntimeEvent::Checkpoint(CheckpointEvent::CheckpointCreated(
-          _,
-          _,
-          id,
-          ..
-        )) => {
-          ids.push(CreatedIds::CheckpointCreated(id.clone()));
-        }
-        RuntimeEvent::Checkpoint(CheckpointEvent::ScheduleCreated(
-          _,
-          _,
-          id,
-          ..
-        )) => {
-          ids.push(CreatedIds::ScheduleCreated(id.clone()));
-        }
-        RuntimeEvent::Identity(IdentityEvent::DidCreated(
-          id,
-          ..
-        )) => {
-          ids.push(CreatedIds::IdentityCreated(*id));
-        }
-        RuntimeEvent::Identity(IdentityEvent::ChildDidCreated(
-          _,
-          id,
-          ..
-        )) => {
-          ids.push(CreatedIds::ChildIdentityCreated(*id));
-        }
-        RuntimeEvent::MultiSig(MultiSigEvent::MultiSigCreated(
-          _,
-          id,
-          ..
-        )) => {
-          ids.push(CreatedIds::MultiSigCreated(*id));
+        RuntimeEvent::Identity(IdentityEvent::ChildDidCreated(_, id, ..)) => {
+          return Some(*id);
         }
         _ => (),
       }
     }
-    ids
-  }).unwrap_or_default())
+    None
+  }))
 }
 
 /// Search transaction events for VenueId.
@@ -106,11 +100,7 @@ pub async fn get_venue_id(mut res: TransactionResults) -> Result<Option<VenueId>
   Ok(res.events().await?.and_then(|events| {
     for rec in &events.0 {
       match &rec.event {
-        RuntimeEvent::Settlement(SettlementEvent::VenueCreated(
-          _,
-          venue_id,
-          ..
-        )) => {
+        RuntimeEvent::Settlement(SettlementEvent::VenueCreated(_, venue_id, ..)) => {
           return Some(*venue_id);
         }
         _ => (),
@@ -125,12 +115,7 @@ pub async fn get_instruction_id(mut res: TransactionResults) -> Result<Option<In
   Ok(res.events().await?.and_then(|events| {
     for rec in &events.0 {
       match &rec.event {
-        RuntimeEvent::Settlement(SettlementEvent::InstructionCreated(
-          _,
-          _,
-          instruction_id,
-          ..
-        )) => {
+        RuntimeEvent::Settlement(SettlementEvent::InstructionCreated(_, _, instruction_id, ..)) => {
           return Some(*instruction_id);
         }
         _ => (),
